a2adb9223771e0667ab80c8a287cc7aad2dc111e
Rachel <rmoshier@gmail.com>
2015-01-06 14:30:03 -0800

    created js file

---
+++ index.js
@@ -0,0 +1,14 @@
+var createGame = require('voxel-engine');
+
+var game = createGame(
+  texturePath: 'textures/'
+);
+
+var container = document.body;
+game.appendTo(container);
+
+var createPlayer = require('voxel-player')(game);
+
+var momo = createPlayer('momo.png');
+momo.possess();
+momo.yaw.position.set(0, 100, 0);
